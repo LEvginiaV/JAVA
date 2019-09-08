791370de5fc65380daa79752e7d2b90ff28c8448
Evgenia Lopatina <lev.png@bk.ru>
2019-09-09 01:21:51 +0300

    Выполнена домашка №2

--- homework_2/src/main/java/Point.java
+++ homework_2/src/main/java/Point.java
@@ -1,6 +1,16 @@
+import com.sun.corba.se.impl.oa.poa.POAImpl;
+
 public class Point {
 
-    public static void main (String[] args) {
-        System.out.println("test");
+    public double a;
+    public double b;
+
+    public Point (double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double distance(Point p2) {
+        return Math.sqrt(Math.pow(this.a - p2.a, 2) + Math.pow(this.b - p2.b, 2));
     }
 }
