6593218c866c2504522512d5256b5212f35455be
Evgenia Lopatina <lev.png@bk.ru>
2019-09-09 23:48:44 +0300

    Задание 2 перенесено в проект "sandbox"

--- sandbox/src/main/java/ru/Lopatina/JAVA_lessons/sandbox/Point.java
+++ sandbox/src/main/java/ru/Lopatina/JAVA_lessons/sandbox/Point.java
@@ -1,11 +1,11 @@
-import com.sun.corba.se.impl.oa.poa.POAImpl;
+package ru.Lopatina.JAVA_lessons.sandbox;
 
 public class Point {
 
     public double a;
     public double b;
 
-    public Point (double a, double b) {
+    public Point(double a, double b) {
         this.a = a;
         this.b = b;
     }
