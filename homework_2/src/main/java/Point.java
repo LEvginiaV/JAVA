686945877b8befcfedfaccee9a32663f0fd366f1
Evgenia Lopatina <lev.png@bk.ru>
2019-09-10 00:36:01 +0300

    Revert "Выполнена домашка №2"
    This reverts commit 791370de5fc65380daa79752e7d2b90ff28c8448.

--- homework_2/src/main/java/Point.java
+++ homework_2/src/main/java/Point.java
@@ -1,16 +1,6 @@
-import com.sun.corba.se.impl.oa.poa.POAImpl;
-
 public class Point {
 
-    public double a;
-    public double b;
-
-    public Point (double a, double b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    public double distance(Point p2) {
-        return Math.sqrt(Math.pow(this.a - p2.a, 2) + Math.pow(this.b - p2.b, 2));
+    public static void main (String[] args) {
+        System.out.println("test");
     }
 }
