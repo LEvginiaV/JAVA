5f603e3cc3c86d40aefcc84e5bc10cbdcbac76ff
Evgenia Lopatina <lev.png@bk.ru>
2019-09-18 01:35:16 +0300

    Задание 4 выполнено

--- addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/ContactCreationTests.java
@@ -1,17 +1,21 @@
 package ru.Lopatina.addressbook;
 
-import java.io.File;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class ContactCreationTests {
   private WebDriver wd;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -108,7 +112,7 @@ public class ContactCreationTests {
     wd.findElement(By.linkText("add new")).click();
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
   }
