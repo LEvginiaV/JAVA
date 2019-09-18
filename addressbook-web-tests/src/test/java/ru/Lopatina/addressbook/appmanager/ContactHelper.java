3079cf38ddadc852aba1cb7425176099bcf9a13d
Evgenia Lopatina <lev.png@bk.ru>
2019-09-19 00:51:06 +0300

    выделен спец класс ContactHelper

---
+++ addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,81 @@
+package ru.Lopatina.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import ru.Lopatina.addressbook.model.Date;
+
+public class ContactHelper {
+    private WebDriver wd;
+
+    public ContactHelper(WebDriver wd) {
+        this.wd = wd;
+    }
+
+    public void returnToHomePage() {
+      wd.findElement(By.linkText("home page")).click();
+    }
+
+    public void submitContactCreation() {
+      wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    public void fillNotes(String Notes) {
+      wd.findElement(By.name("notes")).sendKeys(Notes);
+    }
+
+    public void fillHomeContacts(String homeAdress, String homePhone) {
+      wd.findElement(By.name("address2")).sendKeys(homeAdress);
+      wd.findElement(By.name("phone2")).sendKeys(homePhone);
+    }
+
+    public void chooseGroup(String groupTitle) {
+      new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(groupTitle);
+    }
+
+    public void fillDates(Date birthDate, Date anniversaryDate) {
+      new Select(wd.findElement(By.name("bday"))).selectByVisibleText(birthDate.getDay());
+      new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(birthDate.getMonth());
+      wd.findElement(By.name("byear")).sendKeys(birthDate.getYear());
+      new Select(wd.findElement(By.name("aday"))).selectByVisibleText(anniversaryDate.getDay());
+      new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(anniversaryDate.getMonth());
+      wd.findElement(By.name("ayear")).sendKeys(anniversaryDate.getYear());
+    }
+
+    public void fillWebContacts(String email, String email2, String email3, String homepage) {
+      wd.findElement(By.name("email")).sendKeys(email);
+      wd.findElement(By.name("email2")).sendKeys(email2);
+      wd.findElement(By.name("email3")).sendKeys(email3);
+      wd.findElement(By.name("homepage")).sendKeys(homepage);
+    }
+
+    public void fillPhoneNumbers(String homePhone, String mobilePhone, String workPhone, String fax) {
+      wd.findElement(By.name("home")).sendKeys(homePhone);
+      wd.findElement(By.name("mobile")).sendKeys(mobilePhone);
+      wd.findElement(By.name("work")).sendKeys(workPhone);
+      wd.findElement(By.name("fax")).sendKeys(fax);
+    }
+
+    public void fillAboutWork(String position, String company, String companyAddress) {
+      wd.findElement(By.name("title")).sendKeys(position);
+      wd.findElement(By.name("company")).sendKeys(company);
+      wd.findElement(By.name("address")).sendKeys(companyAddress);
+    }
+
+    public void chooseAvatar(String img) {
+      String dir = new String(System.getProperty("user.dir"));
+      System.out.println(dir);
+      wd.findElement(By.name("photo")).sendKeys(dir+img);
+    }
+
+    public void fillNameForms(String firstName, String middleName, String lastName, String nickName) {
+      wd.findElement(By.name("firstname")).sendKeys(firstName);
+      wd.findElement(By.name("middlename")).sendKeys(middleName);
+      wd.findElement(By.name("lastname")).sendKeys(lastName);
+      wd.findElement(By.name("nickname")).sendKeys(nickName);
+    }
+
+    public void initContactCreation() {
+      wd.findElement(By.linkText("add new")).click();
+    }
+}
--- addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/tests/ContactCreationTests.java
@@ -8,17 +8,17 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-    app.initContactCreation();
-    app.fillNameForms("Gheorghe", "Alan", "Smith", "Nicky");
-    app.chooseAvatar("\\img\\i380664.jpg");
-    app.fillAboutWork("Tester", "Kontur", "Leninskiy avenu,168");
-    app.fillPhoneNumbers("7-09-46", "8-924-345-23-34", "345-45-35", "234-45-23");
-    app.fillWebContacts("Email1@mail.ru", "Email2@bk.ru", "Email3@gmail.ru", "vk.com");
-    app.fillDates(new Date("12", "March", "1995"), new Date("11", "June", "2001"));
-    app.chooseGroup("test1");
-    app.fillHomeContacts("SPb, Nevsky avenu", "345-56-34");
-    app.fillNotes("Fish seller");
-    app.submitContactCreation();
-    app.returnToHomePage();
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillNameForms("Gheorghe", "Alan", "Smith", "Nicky");
+    app.getContactHelper().chooseAvatar("\\img\\i380664.jpg");
+    app.getContactHelper().fillAboutWork("Tester", "Kontur", "Leninskiy avenu,168");
+    app.getContactHelper().fillPhoneNumbers("7-09-46", "8-924-345-23-34", "345-45-35", "234-45-23");
+    app.getContactHelper().fillWebContacts("Email1@mail.ru", "Email2@bk.ru", "Email3@gmail.ru", "vk.com");
+    app.getContactHelper().fillDates(new Date("12", "March", "1995"), new Date("11", "June", "2001"));
+    app.getContactHelper().chooseGroup("test1");
+    app.getContactHelper().fillHomeContacts("SPb, Nevsky avenu", "345-56-34");
+    app.getContactHelper().fillNotes("Fish seller");
+    app.getContactHelper().submitContactCreation();
+    app.getContactHelper().returnToHomePage();
   }
 }
