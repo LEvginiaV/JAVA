3a6467ce8d02a21c2f871c7f1e953bd188a3bb25
Evgenia Lopatina <lev.png@bk.ru>
2019-10-02 01:19:41 +0300

    выполнена лекция 4.7

--- addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/tests/GroupModificationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.Lopatina.addressbook.model.GroupData;
 import ru.Lopatina.addressbook.model.TestBase;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -19,11 +20,16 @@ public class GroupModificationTests extends TestBase {
     //int before = app.getContactHelper().getGroupCount();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     //int after = app.getContactHelper().getGroupCount();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
