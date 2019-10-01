2d224cfcd996a2c3946cf1ef64d476dd2c03053c
Evgenia Lopatina <lev.png@bk.ru>
2019-10-02 00:16:33 +0300

    выполнена лекция 4.6

--- addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/tests/GroupDeletionTests.java
@@ -23,6 +23,9 @@ public class GroupDeletionTests extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     //int after = app.getContactHelper().getGroupCount();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 
 }
