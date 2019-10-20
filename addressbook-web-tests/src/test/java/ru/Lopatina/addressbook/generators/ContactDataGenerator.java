03dd3beb834f90b87283d3bb39550793b3bba396
Evgenia Lopatina <lev.png@bk.ru>
2019-10-21 02:26:59 +0300

    Сделаны лекции 6.8-6.9

--- addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/generators/ContactDataGenerator.java
@@ -54,18 +54,18 @@ public class ContactDataGenerator {
   private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file);) {
+      writer.write(json);
+    }
   }
 
   private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
   }
 
   private List<ContactData> generateContacts(int count) {
@@ -92,17 +92,17 @@ public class ContactDataGenerator {
   }
 
   private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    for (ContactData contact : contacts) {
-      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n",
-              contact.getFirstName(), contact.getMiddleName(), contact.getLastName(),
-              contact.getNickName(), contact.getPhoto(), contact.getPosition(), contact.getCompany(),
-              contact.getCompanyAddress(), contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(),
-              contact.getFax(), contact.getEmail(), contact.getEmail(), contact.getEmail2(), contact.getEmail3(),
-              contact.getHomepage(), contact.getBday(), contact.getBmonth(), contact.getByear(), contact.getAday(),
-              contact.getAmonth(), contact.getAyear(), contact.getGroup(), contact.getHomeAddress(),
-              contact.getHomePhone2(), contact.getPosition(), contact.getNotes()));
+    try (Writer writer = new FileWriter(file)) {
+      for (ContactData contact : contacts) {
+        writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n",
+                contact.getFirstName(), contact.getMiddleName(), contact.getLastName(),
+                contact.getNickName(), contact.getPhoto(), contact.getPosition(), contact.getCompany(),
+                contact.getCompanyAddress(), contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(),
+                contact.getFax(), contact.getEmail(), contact.getEmail(), contact.getEmail2(), contact.getEmail3(),
+                contact.getHomepage(), contact.getBday(), contact.getBmonth(), contact.getByear(), contact.getAday(),
+                contact.getAmonth(), contact.getAyear(), contact.getGroup(), contact.getHomeAddress(),
+                contact.getHomePhone2(), contact.getPosition(), contact.getNotes()));
+      }
     }
-    writer.close();
   }
 }
--- addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/Lopatina/addressbook/tests/ContactCreationTests.java
@@ -7,14 +7,12 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.Lopatina.addressbook.model.ContactData;
 import ru.Lopatina.addressbook.model.Contacts;
-import ru.Lopatina.addressbook.model.GroupData;
 import ru.Lopatina.addressbook.model.TestBase;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,35 +24,38 @@ public class ContactCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validContactsFromXml() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.xml")));
-    String xml = "";
-    String line = reader.readLine();
-    while (line != null) {
-      xml += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.xml")))) {
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(ContactData.class);
+      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+      return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
-    XStream xstream = new XStream();
-    xstream.processAnnotations(ContactData.class);
-    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @DataProvider
   public Iterator<Object[]> validContactsFromJson() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.json")));
-    String json = "";
-    String line = reader.readLine();
-    while (line != null) {
-      json += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.json")))) {
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+      return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
-    Gson gson = new Gson();
-    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
-    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validContactsFromJson")
   public void testContactCreation(ContactData contact) throws Exception {
+    app.goTo().homePage();
     Contacts before = app.contact().all();
     app.contact().create(contact, true);
     Contacts after = app.contact().all();
